b4d35bb5f533148756272cbe99a879f7aaee65a9
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-09 11:59:27 -0300

    Implementação do endpoint read all

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000);
+// Heróis e Heroínas
+
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//               0                    1               2
+
+// Read All (Ler todos os itens)
+app.get("/herois", function (req, res) {
+  res.send(herois);
+});
+
+app.listen(3000, () =>
+  console.log("Aplicação rodando em http://localhost:3000")
+);
