78b7dbd6b8d509c1f23b2b8a79d9cd025790cdac
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-10 10:19:51 -0300

    Criação de registros implementada

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+// Indica para o Express que estamos utilizando JSON na requisições
+app.use(express.json());
+
+// Criação do endpoint principal
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
@@ -13,6 +17,17 @@ const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
 // Read All (Ler todos os itens)
 app.get("/herois", function (req, res) {
   res.send(herois);
+});
+
+// Create (Criar um item)
+app.post("/herois", function (req, res) {
+  // Obtemos o nome que foi enviado no body da requisição
+  const item = req.body.nome;
+
+  // Adicionamos esse item obtido dentro da lista de heróis
+  herois.push(item);
+
+  res.send("Item criado com sucesso!");
 });
 
 app.listen(3000, () =>
